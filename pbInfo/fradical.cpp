4a9866255baa3a9b449bcc55ac9cc822e4b41a0e
Goța Septimiu Andrei <111377955+AndreiGota@users.noreply.github.com>
2023-06-29 22:52:30 +0300

    Add files via upload

---
+++ pbInfo/fradical.cpp
@@ -0,0 +1,10 @@
+void FRadical(int n, int &x, int &y) {
+    for (y = 1; y <= n; y++) {
+        if (n % y == 0) {
+            if (sqrt(n / y) == (int)(sqrt(n / y))) {
+                x = sqrt(n / y);
+                break;
+            }
+        }
+    }
+}
